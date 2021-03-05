8ca403d8057109427f7eca080c5c419ec71d6ad7
Olumuyiwa Osiname <olumuyiwa.osiname@homeday.de>
2021-03-05 18:10:47 +0100

    add model scopes to event

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,6 +4,18 @@ class Event < ApplicationRecord
   belongs_to :competition
   has_many :odds, dependent: :destroy
 
+  scope :future, -> { where("starts_at > ?", Time.now) }
+
+  scope :by_competition, -> (competition_name){
+    future.joins(:competition).where("LOWER(competitions.name) =  ?", competition_name.downcase)
+  }
+
+  scope :by_country, -> (country){
+    future.joins(:competition).where("LOWER(competitions.country) =  ?", country.downcase)
+  }
+
+  scope :by_date, -> (date) { future.where("DATE(starts_at) = ?",date) }
+
   def set_top_odds
     home = odds.maximum(:home)
     draw = odds.maximum(:draw)
