40b8f904c51c1b812727b2e70d489c215be4aa5f
Olumuyiwa Osiname <olumuyiwa.osiname@homeday.de>
2021-03-06 18:42:50 +0100

    Authenticate events and split production keys

--- app/controllers/v1/events_controller.rb
+++ app/controllers/v1/events_controller.rb
@@ -1,4 +1,6 @@
 class V1::EventsController < ApplicationController
+  before_action :authenticate
+  
   def index
     relation = Event.includes(:competition).future
     relation = relation.by_date(params[:date]) if params[:date]
@@ -12,5 +14,19 @@ class V1::EventsController < ApplicationController
     @event = Event.find_by(uid: params[:id])
 
     return render staus: 404 unless @event
+  end
+
+  private
+
+  def authenticate
+    return head :unauthorized unless ActiveSupport::SecurityUtils.secure_compare(token, Rails.application.credentials.api_key)
+  end
+
+  def token
+    if request.headers['Authorization'].present?
+      return request.headers['Authorization'].split(' ').last
+    else
+      return head :unauthorized
+    end
   end
 end
