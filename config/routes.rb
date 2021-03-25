2e0ae45b1646ad29d16dd973a3e34688b0245d04
Olumuyiwa <oluosiname@gmail.com>
2021-03-25 10:29:23 +0100

    change sidekiq auth

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   Sidekiq::Web.use(Rack::Auth::Basic) do |username, password|
-    username == Rails.application.credentials[Rails.env.to_sym][:sidekiqweb][:username] &&
-    password == Rails.application.credentials[Rails.env.to_sym][:sidekiqweb][:password]
+    username == Rails.application.credentials[:sidekiqweb][:username] &&
+    password == Rails.application.credentials[:sidekiqweb][:password]
   end
 
   mount Sidekiq::Web => "/sidekiq"
