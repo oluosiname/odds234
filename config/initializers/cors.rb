b84df90edac36b27dfa08b92d73d5ec1e2c63998
Olumuyiwa Osiname <oluosiname@gmail.com>
2022-02-05 16:18:52 +0100

    set dev credentials and cors fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -12,4 +12,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins 'https://www.234odds.com'
     resource '*', headers: :any, methods: [:get, :post]
   end
+
+  allow do
+    origins 'http://localhost:3000'
+    resource '*', headers: :any, methods: [:get]
+  end
 end
